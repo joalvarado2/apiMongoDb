22899d1819aedf7c3be5c81f78cc26833e1ccbbd
joalvarado2 <jonathanalva92@gmail.com>
2022-01-25 20:01:05 -0500

    agregando get usuario, update y delete

--- src/routes/user.js
+++ src/routes/user.js
@@ -20,4 +20,32 @@ router.get("/users", (req, res) => {
     .catch((error) => res.json({ message: error }));
 });
 
+// get user
+router.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  userSchema
+    .findById(id)
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+// update user
+router.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, age, email } = req.body;
+  userSchema
+    .updateOne({ _id: id }, { $set: { name, age, email } })
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+// delete user
+router.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+  userSchema
+    .remove({ _id: id })
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
 module.exports = router;
